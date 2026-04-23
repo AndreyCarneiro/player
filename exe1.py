8f7380742b6d943f5bc64d4681ffddff72a325af
Andrey <andreycarneiro786@gmail.com>
2026-04-23 17:18:26 -0300

    Última atualização

--- exe1.py
+++ exe1.py
@@ -5,6 +5,12 @@ janela.title("Player")
 janela.geometry("300x200")
 janela.configure(bg= "#000000")
 
+def play():
+    print("Play")
+
+btn = tk.Button(janela, text="Play", command=play, fg = "#962A2A",bg = "#000000" )
+btn.pack()
+
 txt = tk.Label(janela, text ="Teste", fg = "#962A2A",bg = "#000000",font= ("Arial",38,"bold"), padx = 14, pady = 14)
 txt.pack()
 
